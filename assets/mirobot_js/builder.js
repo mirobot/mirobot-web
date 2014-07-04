cdc995a767290ea3e9ead8b8444553a13126bc67
Ben Pirt <ben@pirt.co.uk>
2014-07-04 22:00:22 +0100

    Updated the mirobot ui code

--- assets/mirobot_js/builder.js
+++ assets/mirobot_js/builder.js
@@ -1,6 +1,7 @@
-var Instance = function(fn, el){
+var Instance = function(fn, el, mirobot){
   this.fn = fn;
   this.el = el;
+  this.mirobot = mirobot;
   this.parent = false;
   this.children = []
 }
@@ -10,7 +11,7 @@ Instance.prototype = {
     var self = this;
     if(self.fn){
       // This is a function
-      self.fn.run(self, function(state){ self.updateState(state)});
+      self.fn.run(self, self.mirobot, function(state){ self.updateState(state)});
     }else{
       // This is the root container
       for(var i in self.children){
@@ -44,11 +45,13 @@ Instance.prototype = {
   }
 }
 
-var Builder = function(el){
+var Builder = function(el, mirobot){
   var self = this;
   this.el = el;
+  this.mirobot = mirobot;
   this.init();
   this.fns = {};
+  this.paused = false;
   $.each(this.functions, function(i, f){
     self.fns[f.name] = f;
   });
@@ -60,8 +63,12 @@ Builder.prototype = {
     var self = this;
     var adjustment;
     this.el.addClass('editor');
-    this.library = $('<ol class="library"></ol>');
+    var left = $('<div class="left container"><h2>Toolbox</h2></div>');
+    this.functionList = $('<ol class="functionList"></ol>');
+    left.append(this.functionList);
+    var right = $('<div class="right container"><h2>Program</h2></div>');
     this.program = $('<ol class="program"></ol>');
+    right.append(this.program);
     this.program.sortable({
       group: 'main',
       nested: true,
@@ -95,28 +102,36 @@ Builder.prototype = {
         item.remove();
       }
     });
-    this.library.sortable({
+    this.functionList.sortable({
       group: 'main',
       drop: false,
       exclude: 'input,select'
     });
     
-    this.library.find('input[type=text]').on('click', function(){ jQuery(this).focus(); });
-    
-    this.el.append(this.library);
-    this.el.append(this.program);
-    this.setHeights();
-    $(window).on('resize', function(){ self.setHeights() });
-    var runner = $('<button class="run">Run</button>');
-    runner.on('click', function(e){self.run(e)});
-    this.el.append(runner);
+    this.el.append(left);
+    this.el.append(right);
+    this.runner = $('<button class="run">&#9654; Run</button>');
+    this.pause = $('<button class="pause">&#10074;&#10074; Pause</button>');
+    this.pause.on('click', function(e){self.pauseProgram(e)});
+    this.pause.hide();
+    this.stop = $('<button class="stop">&#9724; Stop</button>');
+    this.clear = $('<button class="clear">&#10006; Clear</button>');
+    this.runner.on('click', function(e){self.runProgram(e)});
+    this.stop.on('click', function(e){self.stopProgram(e)});
+    this.clear.on('click', function(e){self.clearProgram(e)});
+    this.mirobot.addListener(function(state){ self.mirobotHandler(state) });
+    right.append(this.runner);
+    right.append(this.pause);
+    right.append(this.stop);
+    right.append(this.clear);
     
     this.addFunctions();
   },
-  setHeights: function(){
-    var newHeight = $(window).height() - 2 * (1 + 10 + 8);
-    this.library.height(newHeight);
-    this.program.height(newHeight);
+  mirobotHandler: function(state){
+    if(state === 'program_complete'){
+      this.runner.show();
+      this.pause.hide();
+    }
   },
   addFunctions: function(){
     var self = this;
@@ -156,19 +171,50 @@ Builder.prototype = {
         fn.append('<ol></ol>');
       }
 
-      self.el.find('.library').append(fn);
+      self.el.find('.functionList').append(fn);
     });
   },
-  run: function(){
-    this.prog = new Instance(null, null);
-    this.generate(this.el.find('ol.program'), this.prog);
-    this.prog.run()
+  runProgram: function(){
+    if(this.paused){
+      this.mirobot.resume(function(){
+        console.log('resumed');
+      });
+    }else{
+      this.prog = new Instance(null, null, null);
+      this.generate(this.el.find('ol.program'), this.prog);
+      this.prog.run()
+    }
+    this.pause.show();
+    this.runner.hide();
+    this.paused = false;
+  },
+  pauseProgram: function(){
+    var self = this;
+    this.paused = true;
+    this.mirobot.pause(function(){
+      console.log('paused');
+      self.runner.show();
+      self.pause.hide();
+    });
+  },
+  stopProgram: function(){
+    var self = this;
+    this.mirobot.stop(function(){
+      self.runner.show();
+      self.pause.hide();
+      self.paused = false;
+      console.log('stopped');
+    });
+  },
+  clearProgram: function(){
+    this.stopProgram();
+    this.el.find('ol.program li').remove();
   },
   generate: function(el, parent){
     var self = this;
     $(el).children('li').each(function(i, f){
       var fn = self.fns[$(f).data('fntype')];
-      var inst = new Instance(fn, f);
+      var inst = new Instance(fn, f, self.mirobot);
       parent.addChild(inst);
       if(fn.type === 'parent'){
         self.generate($(f).children('ol'), inst);
@@ -176,22 +222,6 @@ Builder.prototype = {
     });
   },
   functions:[
-    {
-      name:'repeat',
-      type:'parent',
-      content:[
-        'Repeat',
-        {input:'number', name:'count', default:2},
-        'times'
-      ],
-      run: function(node, cb){
-        for(var i=0; i< node.args().count; i++){
-          for(var j=0; j< node.children.length; j++){
-            node.children[j].run();
-          }
-        }
-      }
-    },
     {
       name:'move',
       type:'child',
@@ -202,7 +232,7 @@ Builder.prototype = {
         {input:'number', name:'distance', default:100},
         'mm'
       ],
-      run: function(node, cb){
+      run: function(node, mirobot, cb){
         mirobot.move(node.args().direction, node.args().distance, cb);
       }
     },
@@ -216,7 +246,7 @@ Builder.prototype = {
         {input:'number', name:'angle', default:90},
         'degrees'
       ],
-      run: function(node, cb){
+      run: function(node, mirobot, cb){
         mirobot.turn(node.args().direction, node.args().angle, cb);
       }
     },
@@ -224,7 +254,7 @@ Builder.prototype = {
       name:'penup',
       type:'child',
       content:['Pen up'],
-      run: function(node, cb){
+      run: function(node, mirobot, cb){
         mirobot.penup(cb);
       }
     },
@@ -232,8 +262,24 @@ Builder.prototype = {
       name:'pendown',
       type:'child',
       content:['Pen down'],
-      run: function(node, cb){
+      run: function(node, mirobot, cb){
         mirobot.pendown(cb);
+      }
+    },
+    {
+      name:'repeat',
+      type:'parent',
+      content:[
+        'Repeat',
+        {input:'number', name:'count', default:2},
+        'times'
+      ],
+      run: function(node, mirobot, cb){
+        for(var i=0; i< node.args().count; i++){
+          for(var j=0; j< node.children.length; j++){
+            node.children[j].run();
+          }
+        }
       }
     }
   ]
